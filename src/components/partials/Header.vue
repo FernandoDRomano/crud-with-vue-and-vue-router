4afe9d04be1a357608c4fe36828c2951e4d2815d
Fernando Daniel Romano <fernandoreceptor2014@gmail.com>
2021-07-13 11:57:54 -0300

    Creando el Header y la Navegacion

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
-        header
+        <div class="bg-blue-500 py-5 flex items-center justify-center">
+            <img alt="Vue logo" src="../../assets/logo.png" class="h-12">
+            <h1 class="text-center text-white text-2xl font-light">Crud con VueRouter - Vuex y JsonServer</h1>
+        </div>
+        <nav-vue></nav-vue>
     </div>
 </template>
 
 <script>
+    import NavVue from './Nav.vue'
     export default {
-        name: 'Header'
+        name: 'Header',
+        components:{
+            NavVue
+        }
     }
 </script>
